e46f5e1d91d8afb4df7874dee10ab2e3e6660e70
RK <robert.kehoe@singingbottle.com>
2019-07-13 20:00:39 +0200

    footer reduce

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,30 +12,11 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
 
-              </div>
-              <div className="column is-4 social">
-                <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                <Link className="navbar-item" to="/blog">
-                        Archive
-                      </Link>
-              </div>
-            </div>
+        </div>
+        <div className="">
+          <div className="">
+
           </div>
         </div>
       </footer>
