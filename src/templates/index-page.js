b002a1888d2460314142f61601386b5ba8ff2085
RK <robert.kehoe@singingbottle.com>
2019-07-14 00:27:20 +0200

    bul

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -36,12 +36,11 @@ export const IndexPageTemplate = ({
         }}
       >
         <h1
-          className="has-text-weight-bold"
+          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             color: 'black',
             lineHeight: '1',
             padding: '0.25em',
-            fontSize: '92px !important',
           }}
         >
           {title}
