837abf080b203dcea18f5e785749c96e29588c1f
RK <robert.kehoe@singingbottle.com>
2019-07-14 12:11:46 +0200

    ..

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Video = ({ videos, gridItems, ...props }) => (
+const Video = ({ videos, gridItems }) => (
     {gridItems.map(item => (
   <div className="video">
     <iframe
-      src={videoSrcURL}
-      title={videoTitle}
+      src={item.videoSrcURL}
+      title={item.videoTitle}
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       frameBorder="0"
       webkitallowfullscreen="true"
@@ -17,3 +17,4 @@ const Video = ({ videos, gridItems, ...props }) => (
 )
 
 export default Video
+
